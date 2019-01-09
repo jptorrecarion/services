58444db648345e8e5e29803aba2b3528fb883f34
Christian <christianjerr@yahoo.com>
2019-01-10 00:36:26 +0800

    Updates

---
+++ precache-manifest.8b1398a421d486e57f94f296ff8caae4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "992a1f1654edd03bc9271b875b16adb4",
+    "url": "/static/media/techTwo.992a1f16.jpg"
+  },
+  {
+    "revision": "3a3de718225a841ae4cc54d479ec8391",
+    "url": "/static/media/tech.3a3de718.jpg"
+  },
+  {
+    "revision": "88b16ff155ef815ccd4652e1b8447ded",
+    "url": "/static/media/jp.88b16ff1.docx"
+  },
+  {
+    "revision": "bee36bb775304128ae2e20fcb30e4772",
+    "url": "/static/media/backend.bee36bb7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9e3f679d758a37b6627",
+    "url": "/static/js/main.a9e3f679.chunk.js"
+  },
+  {
+    "revision": "c9c4a746700b3d817383",
+    "url": "/static/js/1.c9c4a746.chunk.js"
+  },
+  {
+    "revision": "a9e3f679d758a37b6627",
+    "url": "/static/css/main.bb0ee588.chunk.css"
+  },
+  {
+    "revision": "24d690322c8356795cdd5d7158803f99",
+    "url": "/index.html"
+  }
+];
